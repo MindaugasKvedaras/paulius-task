1001b2bd721a20631926b76d6f6cbd25bb61bb0d
Minkcode <kvedaras.mindaugas@gmail.com>
2022-10-21 22:14:41 +0300

    navbar done

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import "./Navbar.css";
+import { LT } from 'country-flag-icons/react/3x2';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import { BsQuestionCircle } from 'react-icons/bs';
+import { HiOutlineUser } from 'react-icons/hi';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+        <div className="navbar_left-items">
+            <div className="navbar_left-items_blue-circle"/>
+            <div className="navbar_left-items_language_help">
+                <div className="language">
+                    <LT className="language_flag"/>
+                    <p className="text">LT</p>
+                    <MdOutlineKeyboardArrowDown className="arrow"/>
+                </div>
+                <div className="help">
+                    <BsQuestionCircle className="help_question"/>
+                    <p className="text">Pagalba</p>
+                    <MdOutlineKeyboardArrowDown className="arrow"/>
+                </div>
+            </div>
+        </div>
+        <div className="navbar_right-items">
+            <HiOutlineUser className="navbar_right-items_user"/>
+            <p className="text">Prisijungti</p>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
